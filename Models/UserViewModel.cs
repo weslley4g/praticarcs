4a5e4cff96bae517da6647bf55cb3c60177f591b
weslley4g <zanirate90@gmail.com>
2021-08-01 11:29:49 -0300

    init project login mvc

---
+++ Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+namespace Name
+{
+    public class User
+    {
+        public int Id {get; set;}
+
+        public string Name {get; set;}
+
+        public string Password {get; set;}
+        
+    }
+}
